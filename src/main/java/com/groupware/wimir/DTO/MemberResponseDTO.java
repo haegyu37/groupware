7ccf9a3abae4c6759e77ec704bb23285beb89cf3
주촌면 파란주먹 <121841731+haegyu37@users.noreply.github.com>
2023-07-13 14:36:59 +0900

    병규씨꺼랑 합친고

--- src/main/java/com/groupware/wimir/DTO/MemberResponseDTO.java
+++ src/main/java/com/groupware/wimir/DTO/MemberResponseDTO.java
@@ -1,7 +1,10 @@
-package com.groupware.wimir.DTO;
+package com.groupware.wimir.dto;
 
-import com.groupware.wimir.entity.Member;
+import com.groupware.wimir.constant.Authority;
+import com.groupware.wimir.entity.*;
 import lombok.*;
+
+import java.util.List;
 
 
 //Response를 보낼때 쓰이는 dto다.
@@ -10,19 +13,26 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class MemberResponseDTO {
-    private String no;
+    private Long id;
+    private Long no;
     private String name;
+//    private String password;
     private String position;
     private String part;
     private String team;
+    private Authority authority;
+
 
     public static MemberResponseDTO of(Member member) {
         return MemberResponseDTO.builder()
                 .no(member.getNo())
                 .name(member.getName())
+//                .password(member.getPassword())
                 .position(member.getPosition())
                 .part(member.getPart())
                 .team(member.getTeam())
+                .authority(member.getAuthority())
                 .build();
     }
+
 }
