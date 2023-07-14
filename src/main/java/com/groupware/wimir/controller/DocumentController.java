2fb79388a589ad616d14a0bd8fe56eb0d4121c13
wrightwright96 <30stmsos@gmail.com>
2023-07-14 11:44:59 +0900

    0714(api주소바꿈)
    .

--- src/main/java/com/groupware/wimir/controller/DocumentController.java
+++ src/main/java/com/groupware/wimir/controller/DocumentController.java
@@ -26,37 +26,62 @@ public class DocumentController {
     private DocumentRepository documentRepository;
 
     // 문서 목록(메인)
-    @GetMapping("/list")
-    public String list(@PageableDefault Pageable pageable, Model model) {
+    @GetMapping
+    public String mainDocument(@PageableDefault Pageable pageable, Model model) {
         model.addAttribute("documentList", documentService.findDocumentList(pageable));
         return "/document/list";
     }
 
     // 문서 조회
-    @GetMapping({"", "/"})
+    @GetMapping("/read")
     public String readDocument(@RequestParam(value = "id", defaultValue = "0") Long id, Model model) {
         model.addAttribute("document", documentService.findDocumentById(id));
-        return "/document/form";
+        return "/document/read";
     }
 
-    // 문서 작성
-    @PostMapping
-    public ResponseEntity<?> createDocument(@RequestBody Document document) {
+    // 문서 작성(작성 후, 작성글 조회)
+    @PostMapping("/write")
+    public String createDocument(@RequestBody Document document) {
         document.setCreateDate(LocalDateTime.now());
-        documentRepository.save(document);
-        return new ResponseEntity<>("{}", HttpStatus.CREATED);
+        Document savedDocument = documentRepository.save(document);
+        return "redirect:/document/read?id=" + savedDocument.getId();
     }
 
-    // 문서 수정
-    @PutMapping("/{id}")
-    public ResponseEntity<?> updateDocument(@PathVariable("id") Long id, @RequestBody Document document) {
+//    // 문서 작성
+//    @PostMapping
+//    public ResponseEntity<?> createDocument(@RequestBody Document document) {
+//        document.setCreateDate(LocalDateTime.now());
+//        documentRepository.save(document);
+//        return new ResponseEntity<>("{}", HttpStatus.CREATED);
+//    }
+
+//    // 문서 수정
+//    @PutMapping("/{id}")
+//    public ResponseEntity<?> updateDocument(@PathVariable("id") Long id, @RequestBody Document document) {
+//        Document updateDocument = documentRepository.getOne(id);
+//        updateDocument.setTitle(document.getTitle());
+//        updateDocument.setContent(document.getContent());
+//        updateDocument.setUpdateDate(LocalDateTime.now());
+//        documentRepository.save(updateDocument);
+//
+//        return new ResponseEntity<>("{}", HttpStatus.OK);
+//    }
+
+    // 문서 수정(조회 페이지에서 수정)
+    @GetMapping("/read/update")
+    public String updateDocumentPage(@RequestParam(value = "id", defaultValue = "0") Long id, Model model) {
+        model.addAttribute("document", documentService.findDocumentById(id));
+        return "/document/update";
+    }
+
+    @PostMapping("/read/update")
+    public String updateDocument(@RequestParam(value = "id", defaultValue = "0") Long id, @RequestBody Document document) {
         Document updateDocument = documentRepository.getOne(id);
         updateDocument.setTitle(document.getTitle());
         updateDocument.setContent(document.getContent());
         updateDocument.setUpdateDate(LocalDateTime.now());
         documentRepository.save(updateDocument);
-
-        return new ResponseEntity<>("{}", HttpStatus.OK);
+        return "redirect:/document/read?id=" + updateDocument.getId();
     }
 
     // 문서 삭제
