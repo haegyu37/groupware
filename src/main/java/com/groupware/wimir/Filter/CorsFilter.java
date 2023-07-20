eb5ee26e004f183ac05a2378f06225b3ad5e3747
주촌면 파란주먹 <121841731+haegyu37@users.noreply.github.com>
2023-07-20 13:20:17 +0900

    병규씨꺼랑 합친거

--- src/main/java/com/groupware/wimir/Filter/CorsFilter.java
+++ src/main/java/com/groupware/wimir/Filter/CorsFilter.java
@@ -1,33 +1,33 @@
-//package com.groupware.wimir.Filter;
-//
-//import org.springframework.core.Ordered;
-//import org.springframework.core.annotation.Order;
-//import org.springframework.stereotype.Component;
-//
-//import javax.servlet.*;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//@Component
-//@Order(Ordered.HIGHEST_PRECEDENCE)
-//public class CorsFilter implements Filter {
-//    @Override
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-//        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-//        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-//
-//        httpServletResponse.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-//        httpServletResponse.setHeader("Access-Control-Allow-Credentials","true");
-//        httpServletResponse.setHeader("Access-Control-Allow-Methods","*");
-//        httpServletResponse.setHeader("Access-Control-Allow-Max-Age","3600");
-//        httpServletResponse.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-//
-//        if ("OPTIONS".equalsIgnoreCase(httpServletRequest.getMethod())){
-//            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
-//        } else {
-//            chain.doFilter(request, response);
-//        }
-//
-//    }
-//}
+package com.groupware.wimir.Filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        httpServletResponse.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials","true");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods","*");
+        httpServletResponse.setHeader("Access-Control-Allow-Max-Age","3600");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+
+        if ("OPTIONS".equalsIgnoreCase(httpServletRequest.getMethod())){
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(request, response);
+        }
+
+    }
+}
