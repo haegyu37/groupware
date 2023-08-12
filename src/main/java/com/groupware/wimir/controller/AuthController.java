d299637c0aee0f0c20a96bd69f3edc4202fb0514
우병규 <dnqudrb94@naver.com>
2023-08-12 16:05:37 +0900

    어제걸로 구

--- src/main/java/com/groupware/wimir/controller/AuthController.java
+++ src/main/java/com/groupware/wimir/controller/AuthController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -41,35 +42,7 @@ public class AuthController {
     private final MemberService memberService;
     private final DocumentService documentService;
 
-    //직원 등록
-    @PostMapping("/admin/signup")
-    public ResponseEntity<MemberResponseDTO> signup(@RequestBody MemberRequestDTO requestDto) {
 
-        return ResponseEntity.ok(authService.signup(requestDto));
-    }
-
-    //직원 목록
-    @GetMapping("/admin/members")
-    public ResponseEntity<List<MemberResponseDTO>> getAllMembers() {
-        List<MemberResponseDTO> members = memberService.getAllMembers();
-        return ResponseEntity.ok(members);
-    }
-
-    //직원 삭제
-    @DeleteMapping("/admin/members/{memberId}")
-    public ResponseEntity<String> deleteMemberById(@PathVariable Long memberId) {
-        try {
-            Optional<Member> memberOptional = memberRepository.findById(memberId);
-            if (memberOptional.isPresent()) {
-                memberRepository.delete(memberOptional.get());
-                return ResponseEntity.ok("회원 정보가 삭제되었습니다.");
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
 
     //로그인
     @PostMapping("/login")
@@ -107,93 +80,11 @@ public class AuthController {
         return ResponseEntity.ok(authService.refresh(tokenRequestDTO));
     }
 
-    //해당 사원 정보보기
-    @GetMapping("/admin/members/{id}")
-    public ResponseEntity<MemberResponseDTO> getMemberById(@PathVariable Long id) {
-        Member member = memberService.getMemberById(id);
-        MemberResponseDTO memberResponseDTO = MemberResponseDTO.of(member);
-        return ResponseEntity.ok(memberResponseDTO);
-    }
-
-    @PostMapping("/admin/members/{memberId}/edit")
-    public ResponseEntity<MemberResponseDTO> changeUserDetails(
-            @PathVariable Long memberId,
-            @RequestPart(name = "image", required = false) MultipartFile image,
-            @RequestPart(name = "changeRequest", required = false) ChangeUserDTO changeRequest
-    ) {
-        try {
-            // 사진 등록
-            boolean imageUploaded = false;
-            if (image != null) {
-                String uploadDir = "src/main/resources/static/images";
-                String fileName = "profile_" + memberId + "." + FilenameUtils.getExtension(image.getOriginalFilename());
-                File file = new File(uploadDir + "/" + fileName);
-                FileUtils.writeByteArrayToFile(file, image.getBytes());
-
-                Member member = memberRepository.findById(memberId)
-                        .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
-                member.setImg("/images/" + fileName);
-                memberRepository.save(member);
-
-                log.info("사진 등록이 완료되었습니다. 사용자 ID: {}", memberId);
-                imageUploaded = true;
-            }
-
-            // 비밀번호 변경
-            boolean passwordChanged = false;
-            if (changeRequest != null && changeRequest.getNewPassword() != null) {
-                String updatedPassword = changeRequest.getNewPassword();
-                MemberResponseDTO updatedUser = memberService.changeUserPasswordByAdmin(memberId, updatedPassword);
-                log.info("비밀번호 변경이 완료되었습니다. 사용자 ID: {}", memberId);
-                passwordChanged = true;
-            }
-
-            // 직급명 변경
-            boolean positionChanged = false;
-            if (changeRequest != null && changeRequest.getPosition() != null) {
-                Position newPosition = changeRequest.getPosition();
-                MemberResponseDTO updatedUser = memberService.changeUserPositionByAdmin(memberId, newPosition);
-                log.info("직급명 변경이 완료되었습니다. 사용자 ID: {}", memberId);
-                positionChanged = true;
-            }
-
-            // 팀명 변경
-            boolean teamChanged = false;
-            if (changeRequest != null && changeRequest.getTeam() != null) {
-                Team newTeam = changeRequest.getTeam();
-                MemberResponseDTO updatedUser = memberService.changeUserTeamByAdmin(memberId, newTeam);
-                log.info("팀명 변경이 완료되었습니다. 사용자 ID: {}", memberId);
-                teamChanged = true;
-            }
-
-            // 변경된 사용자 정보 조회
-            Member updatedMember = memberRepository.findById(memberId)
-                    .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
-
-            MemberResponseDTO responseDTO = MemberResponseDTO.builder()
-                    .id(updatedMember.getId())
-                    .no(updatedMember.getNo())
-                    .name(updatedMember.getName())
-                    .position(updatedMember.getPosition())
-                    .team(updatedMember.getTeam())
-                    .authority(updatedMember.getAuthority())
-                    .img(updatedMember.getImg())
-                    .build();
-
-            return ResponseEntity.ok(responseDTO);
-
-
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-
-    }
-
-
 
 
     // 사용자의 권한을 ROLE_BLOCK으로 업데이트
     @PostMapping("/admin/members/{memberId}/block")
+    @Transactional
     public ResponseEntity<String> blockUser(@PathVariable Long memberId) {
         try {
             Optional<Member> memberOptional = memberRepository.findById(memberId);
