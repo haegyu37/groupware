7791546c8abb12eac1041248a6c90c0456f49ae5
주촌면 파란주먹 <121841731+haegyu37@users.noreply.github.com>
2023-08-24 17:56:21 +0900

    양식 생성자 추가

--- src/main/java/com/groupware/wimir/DTO/TemplateDTO.java
+++ src/main/java/com/groupware/wimir/DTO/TemplateDTO.java
@@ -1,10 +1,13 @@
 package com.groupware.wimir.DTO;
 
 import lombok.*;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Component
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 @Getter
 @Setter
